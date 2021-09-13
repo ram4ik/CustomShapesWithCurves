5f4924c22ab34b833d287eb9acf94b5de804f3bc
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-13 20:20:44 +0300

    Initial Commit

---
+++ CustomShapesWithCurves/CustomShapesWithCurvesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesWithCurvesApp.swift
+//  CustomShapesWithCurves
+//
+//  Created by ramil on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesWithCurvesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
